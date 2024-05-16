dc04414bd23b17be6892196e2e50e18bf2defca2
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-05-16 13:56:25 +0300

    prepare

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
